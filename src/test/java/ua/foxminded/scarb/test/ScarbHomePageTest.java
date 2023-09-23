efaca73f93f266af8aac36d42445f370f34732d4
Gennadii Rostikov <grostikov@gmail.com>
2023-09-23 14:44:51 -0400

    create file config.properties, create class WebDriverFactory (code optimization),create button and title tests

--- src/test/java/ua/foxminded/scarb/test/ScarbHomePageTest.java
+++ src/test/java/ua/foxminded/scarb/test/ScarbHomePageTest.java
@@ -1,39 +1,80 @@
 package ua.foxminded.scarb.test;
 
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import ua.foxminded.scarb.helpers.WebDriverFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class ScarbHomePageTest {
-
-
     private WebDriver driver;
 
-    @BeforeMethod
-    public void setUp() {
-        driver = new ChromeDriver();
-        String baseUrl = "https://skarb.foxminded.ua/";
-        driver.get(baseUrl);
+    @BeforeTest
+    public void setUp() throws IOException {
+         // просим WebDriverFactory создать driver
+         driver = WebDriverFactory.create(); // Chrome работает а EDGE вызывает рандомную ошибку
+
+        // Загрузка файла конфигурации (Вариант 2 без WebDriverFactory)
+//        Properties properties = new Properties();
+//        FileInputStream fileInputStream = new FileInputStream("src/config.properties");
+//        properties.load(fileInputStream);
+//        fileInputStream.close();
+//
+//        // Получение значения браузера из файла конфигурации
+//        String browser = properties.getProperty("browser");
+//
+//        // Инициализация браузера в зависимости от значения
+//        if ("chrome".equalsIgnoreCase(browser)) {
+//            driver = new ChromeDriver();
+//        } else if ("firefox".equalsIgnoreCase(browser)) {
+//            driver = new FirefoxDriver();
+//        } else if ("edge".equalsIgnoreCase(browser)) {
+//            driver = new EdgeDriver();
+//        } else {
+//            System.out.println("Неподдерживаемый браузер: " + browser);
+//            System.exit(1); // Завершаем выполнение программы при неподдерживаемом браузере
+//        }
     }
 
     @Test
     public void getToHomePageTest() {
+        driver.get("https://skarb.foxminded.ua");
+
         WebElement title = driver.findElement(By.cssSelector("h4.text-dark-red"));
         Assert.assertEquals(title.getText(), "SKARB NGO");
     }
+    @Test
+    public void getButtonTextTest() {
+        driver.get("https://skarb.foxminded.ua");
 
-    @AfterMethod(alwaysRun = true)
+        WebElement button = driver.findElement(By.cssSelector("button[name=tasks-button]"));
+        Assert.assertEquals(button.getText(), "Смотреть задачи");
+    }
+
+    @Test
+    public void taskButtonActionTest() {
+        driver.get("https://skarb.foxminded.ua");
+
+        WebElement button = driver.findElement(By.cssSelector("div.statistic button"));
+        button.click();
+        WebElement title = driver.findElement(By.cssSelector("h2"));
+        Assert.assertEquals(title.getText(), "Поиск задач");
+    }
+    @AfterTest
     public void tearDown() {
         if (driver != null) {
-            driver.close();
             driver.quit();
         }
     }
 }
-
