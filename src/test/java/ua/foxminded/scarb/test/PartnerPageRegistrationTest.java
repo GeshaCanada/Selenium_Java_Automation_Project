c7f4e4ed9d1789f9cc677988bff150559fba9efb
Gennadii Rostikov <grostikov@gmail.com>
2023-10-23 13:22:00 -0400

    Window handles added,  code updated = 1 test

--- src/test/java/ua/foxminded/scarb/test/PartnerPageRegistrationTest.java
+++ src/test/java/ua/foxminded/scarb/test/PartnerPageRegistrationTest.java
@@ -11,21 +11,25 @@ import org.testng.annotations.Test;
 import ua.foxminded.scarb.helpers.NotSupportedBrowserException;
 import ua.foxminded.scarb.helpers.WebDriverFactory;
 import utils.RandomDataGenerator2;
-
 import java.time.Duration;
 import java.util.List;
+import java.util.Set;
+
 public class PartnerPageRegistrationTest {
     private WebDriver driver;
     private String baseUrl = "https://skarb.foxminded.ua/";
     private String emailUrl = "https://skarbmail.foxminded.ua/";
+    private String registrationUrl = "https://skarb.foxminded.ua/registration/confirm";
     private String passwordValue = RandomDataGenerator2.generateStrongPassword();
+
     @BeforeMethod
     public void setUp() throws NotSupportedBrowserException {
         driver = WebDriverFactory.create();
         driver.manage().window().maximize();
     }
+
     @Test
-    public void checkPartnerFormTest() {
+    public void checkPartnerFormAndRegistrationPartnerFormTest() {
         driver.get(baseUrl);
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/registration']"))).click();
@@ -42,13 +46,21 @@ public class PartnerPageRegistrationTest {
         inputFields.get(11).sendKeys(RandomDataGenerator2.generateRandomString());
 
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//form//div[3]/button"))).click();
-    }
-    @Test
-    public void checkRegistrationPartnerFormTest() {
+
         driver.get(emailUrl);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[text()='Подтверждение регистрации']//ancestor::span"))).click();
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@href, 'https://skarb.foxminded.ua/registration/confirm')]"))).click();
+
+        Set<String> handles = driver.getWindowHandles();
+        for (String handle : handles) {
+            driver.switchTo().window(handle);
+            if (driver.getCurrentUrl().equals(registrationUrl)) {
+                WebElement page = driver.findElement(By.xpath("//*[contains(text(), 'Ваш email подтверждено')]"));
+                System.out.println(page.getText());
+                break;
+            }
+        }
     }
     @AfterMethod
     public void tearDown() {
