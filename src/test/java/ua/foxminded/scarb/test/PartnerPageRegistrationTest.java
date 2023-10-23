c26ac9bfcba743d806cde6573f893563d390fdd6
Gennadii Rostikov <grostikov@gmail.com>
2023-10-22 21:28:14 -0400

    code optimization+ inputFields

--- src/test/java/ua/foxminded/scarb/test/PartnerPageRegistrationTest.java
+++ src/test/java/ua/foxminded/scarb/test/PartnerPageRegistrationTest.java
@@ -3,22 +3,22 @@ package ua.foxminded.scarb.test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ua.foxminded.scarb.helpers.NotSupportedBrowserException;
 import ua.foxminded.scarb.helpers.WebDriverFactory;
-import utils.RandomDataGenerator;
 import utils.RandomDataGenerator2;
+
 import java.time.Duration;
 import java.util.List;
-
 public class PartnerPageRegistrationTest {
     private WebDriver driver;
     private String baseUrl = "https://skarb.foxminded.ua/";
     private String emailUrl = "https://skarbmail.foxminded.ua/";
+    private String passwordValue = RandomDataGenerator2.generateStrongPassword();
     @BeforeMethod
     public void setUp() throws NotSupportedBrowserException {
         driver = WebDriverFactory.create();
@@ -29,30 +29,17 @@ public class PartnerPageRegistrationTest {
         driver.get(baseUrl);
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/registration']"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(@class, 'btn-success')]"))).click();
 
-        List<WebElement> elements = driver.findElements(By.xpath("//button[contains(@class, 'btn-success')]"));
-        if (!elements.isEmpty()) {
-            elements.get(0).click();
-        }
-
-        String emailPartnerValue = RandomDataGenerator2.generateRandomEmail();
-        String companyNameValue = RandomDataGenerator2.generateRandomString();
-        String firstNameValue = RandomDataGenerator2.generateRandomString();
-        String lastNameValue = RandomDataGenerator2.generateRandomString();
-        String positionNameValue = RandomDataGenerator2.generateRandomString();
-        String passwordValue = RandomDataGenerator.generateStrongPassword();
-
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@type='email']"))).sendKeys(emailPartnerValue);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='organizationName']"))).sendKeys(companyNameValue);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='firstName']"))).sendKeys(firstNameValue);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='lastName']"))).sendKeys(lastNameValue);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='positionInOrganization']"))).sendKeys(positionNameValue);
-
-        WebElement sexBox = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='female']")));
-        sexBox.click();
-
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input#password"))).sendKeys(passwordValue);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input#confirmPassword"))).sendKeys(passwordValue);
+        List<WebElement> inputFields = driver.findElements(By.xpath("//input"));
+        inputFields.get(0).sendKeys(RandomDataGenerator2.generateRandomEmail());
+        inputFields.get(2).sendKeys(RandomDataGenerator2.generateRandomString());
+        inputFields.get(3).sendKeys(RandomDataGenerator2.generateRandomString());
+        inputFields.get(5).click();
+        inputFields.get(6).sendKeys(passwordValue);
+        inputFields.get(7).sendKeys(passwordValue);
+        inputFields.get(8).sendKeys(RandomDataGenerator2.generateRandomString());
+        inputFields.get(11).sendKeys(RandomDataGenerator2.generateRandomString());
 
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//form//div[3]/button"))).click();
     }
