b87bd114bf205a5b9e99078b9ee4c57433194cfb
Gennadii Rostikov <grostikov@gmail.com>
2023-10-25 16:32:16 -0400

    only h5 and li added to each task-tiles

--- src/test/java/ua/foxminded/scarb/test/TaskTilesTest.java
+++ src/test/java/ua/foxminded/scarb/test/TaskTilesTest.java
@@ -25,12 +25,21 @@ public class TaskTilesTest {
     public void printTasksAndCalculateTotalNumber() {
         List<WebElement> taskTiles = driver.findElements(By.name("task-card"));
         System.out.println("Print the tasks into console output:");
-        for (int i = 0; i < taskTiles.size(); i++) {
-            WebElement task = taskTiles.get(i);
-            System.out.println((i + 1) + ". " + task.getText());
+        for (WebElement task : taskTiles) {
+            WebElement title = task.findElement(By.tagName("h5"));
+            System.out.println("Title: " + title.getText());
+            List<WebElement> fields = task.findElements(By.tagName("li"));
+            for (WebElement field : fields) {
+                System.out.println(field.getText());
+            }
+            System.out.println();
         }
         System.out.println("Calculate the overall number of tasks: " + taskTiles.size());
     }
+
+
+
+
 
     @AfterTest
     public void tearDown() {
