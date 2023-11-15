bde42b2d55e8186875ebd0b3e5b91bf6a4f0fe1e
Gennadii Rostikov <grostikov@gmail.com>
2023-11-14 20:52:06 -0500

    Add logging to actions to  tests Volunteer, ScarbHomePage and PartnerPageRegistrationTest , log4j2.xml created,pom.xml updated

--- src/test/java/ua/foxminded/scarb/test/ScarbHomePageTest.java
+++ src/test/java/ua/foxminded/scarb/test/ScarbHomePageTest.java
@@ -3,20 +3,44 @@ package ua.foxminded.scarb.test;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ua.foxminded.scarb.pages.HomePage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class ScarbHomePageTest extends BaseTestNG  {
-    private HomePage pageFactory;
+public class ScarbHomePageTest extends BaseTestNG {
+    private static final Logger LOGGER = LogManager.getLogger(ScarbHomePageTest.class.getName());
+    private HomePage pageHome;
 
     @Test
     public void getToHomePageTest() {
-        pageFactory = new HomePage(driver);
-        Assert.assertEquals(pageFactory.getTitleText(), "SKARB NGO");
+        LOGGER.info("Starting getToHomePageTest");
+
+        pageHome = new HomePage(driver);
+        String actualTitle = pageHome.getTitleText();
+        String expectedTitle = "SKARB NGO";
+
+        LOGGER.debug("Actual Title: " + actualTitle);
+        LOGGER.debug("Expected Title: " + expectedTitle);
+
+        Assert.assertEquals(actualTitle, expectedTitle, "Title mismatch");
+
+        LOGGER.info("getToHomePageTest completed successfully");
     }
 
     @Test
     public void checkContactUsTest() {
-        pageFactory = new HomePage(driver);
-        pageFactory.clickContactUs();
-        Assert.assertEquals(pageFactory.getPageText(), "Свяжитесь с нами");
+        LOGGER.info("Starting checkContactUsTest");
+
+        pageHome = new HomePage(driver);
+        pageHome.clickContactUs();
+        String actualPageText = pageHome.getPageText();
+        String expectedPageText = "Свяжитесь с нами";
+
+        LOGGER.debug("Actual Page Text: " + actualPageText);
+        LOGGER.debug("Expected Page Text: " + expectedPageText);
+
+        Assert.assertEquals(actualPageText, expectedPageText, "Page text mismatch");
+
+        LOGGER.info("checkContactUsTest completed successfully");
     }
 }
+
